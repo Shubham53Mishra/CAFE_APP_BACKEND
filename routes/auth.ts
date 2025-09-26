5eb9f6f716f4c8a3eeb18b4841d337c3f8fb0ebb
Shubham Mishra <codermishra53@gmail.com>
2025-09-26 17:07:19 +0530

    set the url

--- routes/auth.ts
+++ routes/auth.ts
@@ -5,9 +5,15 @@ import User, { IUser } from '../models/User';
 import multer from 'multer';
 import path from 'path';
 
+import fs from 'fs';
+
+const profileImagesDir = path.join(__dirname, '../uploads/profileImages');
+if (!fs.existsSync(profileImagesDir)) {
+  fs.mkdirSync(profileImagesDir, { recursive: true });
+}
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, '../uploads/profileImages'));
+    cb(null, profileImagesDir);
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + '-' + file.originalname);
